8cb7f1fd93cc7589fdd542fa72c2860bfe268788
Const S <umren@yandex.ru>
2020-01-23 20:43:42 +0300

    style fixes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import './App.css';
-import { Card } from './components/Card';
-
-
-
-
+import React from "react";
+import "./App.css";
+import { Card } from "./components/Card";
 
 function App() {
   return (
     <div className="App">
-      <Card title="Name" body="Lorem Ipsum Lorem Ipsum Lorem Ipsum" buttonText="Go somewhere">
-        <img src="https://via.placeholder.com/150"/>
+      <Card
+        title="Name"
+        body="Lorem Ipsum Lorem Ipsum Lorem Ipsum"
+        buttonText="Go somewhere"
+      >
+        <img src="https://via.placeholder.com/150" />
       </Card>
     </div>
   );
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,12 @@
 import React from "react";
 
 export function Card(props) {
-  return <div class="card">
-    {props.children}
-    <h2>{props.title}</h2>
-    <p>{props.body}</p>
-    <button>{props.buttonText}</button>
-  </div>;
+  return (
+    <div class="card">
+      {props.children}
+      <h2>{props.title}</h2>
+      <p>{props.body}</p>
+      <button>{props.buttonText}</button>
+    </div>
+  );
 }
